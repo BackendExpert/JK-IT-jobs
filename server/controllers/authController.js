e972bbc9aadfc8b27ba4fc8f0a58a9cd603cf297
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-18 12:34:45 +0530

    Update
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,59 @@
-const authController = {
+const User = require("../models/User");
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const nodemailer = require('nodemailer');
+const PwdResetToken = require("../models/PwdResetToken");
 
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+    },
+});
+
+const authController = {
+    signup: async (req, res) => {
+        try{
+            const {
+                username, 
+                email,
+                password
+            } = req.body
+
+
+            const checkuser = await User.findOne({
+                $or: [
+                    { username: username },
+                    { email: email },
+                ]
+            })
+
+            if(checkuser){
+                return res.json({ Error: "User Already exists in Given username or Email"})
+            }
+
+            const hashpass = await bcrypt.hash(password, 10)
+
+            const newUser = new User({
+                username: username,
+                email: email,
+                password: hashpass
+            })
+
+            const resultnewUser = await newUser.save()
+
+            if(resultnewUser){
+                return res.json({ Status: "Success"})
+            }   
+            else{
+                return res.json({ Error: "Internal Server Error"})
+            }            
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
 };
 
 module.exports = authController;
