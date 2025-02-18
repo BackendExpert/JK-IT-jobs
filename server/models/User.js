f2ed78192c8902e7997bf84de88420d5f6c5555b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-18 12:24:38 +0530

    Update User.js
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-
-});
+    username: { type: String, required: true, unique: true},
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['admin', 'company'], required: true, default: "company" },  
+}, {timestamps: true });
 
 const User = mongoose.model('User', UserSchema);
 
