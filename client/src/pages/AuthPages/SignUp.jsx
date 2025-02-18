f284712e308d1597c4b5075e9a80d7f49028155d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-18 10:32:20 +0530

    Update SignUp.jsx

--- client/src/pages/AuthPages/SignUp.jsx
+++ client/src/pages/AuthPages/SignUp.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SignUp = () => {
+    const [signupdata, setsignupdata] = useState({
+        username: '', 
+        email: '',
+        password: ''
+    })
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setsignupdata((prevData) => ({
+          ...prevData,
+          [name]: value
+        }));
+    };
+
+    const headlesubmit = (e) => {
+        e.preventDefault()
+    }
+
+
   return (
-    <div>SignUp</div>
+    <div className='min-h-screen bg-[#c0c7d0]'>
+        <div className="xl:py-[10%] xl:px-60 md:px-32 px-4">
+            joasdl
+        </div>    
+    </div>
   )
 }
 
