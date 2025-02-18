9767249ad089c64e3a80419a765628f386071a2d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-18 18:52:51 +0530

    Update authController.js
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,6 +4,8 @@ const jwt = require('jsonwebtoken')
 const nodemailer = require('nodemailer');
 // const PwdResetToken = require("../models/PwdResetToken");
 const validator = require("validator");
+const UserPassOtp = require("../models/UserPassOTP");
+const crypto = require('crypto')
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -103,7 +105,42 @@ const authController = {
                 return res.json({ Error: 'No user found by given email address'})
             }
 
+            const checkotpindb = await UserPassOtp.findOne({ email: email })
+
+            if(checkotpindb){
+                return res.json({ Error: "You Already Request a OTP please try again 15min or check the email"})
+            }
+
+            const otp = Array.from(crypto.randomFillSync(new Uint8Array(6)))
+            .map(byte => "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"[byte % 62])
+            .join('');
             
+            const hashotp = await bcrypt.hash(otp, 10)
+            const defultTime = new Date(); 
+            const expireAt = new Date(defultTime.getTime() + 15 * 60000);
+
+            const newOTP = new UserPassOtp({
+                email: email,
+                otp: hashotp,
+                expire_at: expireAt
+            })
+
+            if(resultOTP){
+                const mailOptions = {
+                    from: process.env.EMAIL_USER,
+                    to: email,
+                    subject: "Password Reset",
+                    html: `<h1>Password Reset OTP</h1>
+                            <p>Password Reset Token: ${otp}</p>
+                            <p>This token will be expired after 15min</p>
+                            <p>Thank you</p>
+                            <p>Admin</p>
+                    `
+                };
+                await transporter.sendMail(mailOptions);
+
+                return res.json({ Status: "Success"})
+            }
         }
         catch(err){
             console.log(err)
