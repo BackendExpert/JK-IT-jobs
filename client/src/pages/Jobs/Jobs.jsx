90fe9092f91b4de0d4d2fb13f4f76f8f113d1f48
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-11 20:29:36 +0530

    Update Jobs.jsx

--- client/src/pages/Jobs/Jobs.jsx
+++ client/src/pages/Jobs/Jobs.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { MdWork } from "react-icons/md";
 
 const Jobs = () => {
   return (
-    <div>Jobs</div>
+    <div className='my-12 mr-4'>
+        <div className="flex">
+            <div className="">
+                <div className="inline-block p-2 bg-[#0f5a97]">
+                    <MdWork className='h-6 w-auto fill-white'/>
+                </div>
+            </div>
+            <div className="pl-4">
+                <h1 className="text-[#0f5a97] text-xl pt-1 font-semibold uppercase">Dashboard</h1>
+            </div>
+        </div>
+    </div>
   )
 }
 
