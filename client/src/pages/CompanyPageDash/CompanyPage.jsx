7d7eb9b117441f4855ed1a31f479d09d4d710fbc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-12 20:50:03 +0530

    Update CompanyPage.jsx

--- client/src/pages/CompanyPageDash/CompanyPage.jsx
+++ client/src/pages/CompanyPageDash/CompanyPage.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BiBuildings } from "react-icons/bi";
 
 const CompanyPage = () => {
   return (
-    <div>CompanyPage</div>
+    <div className='my-12 mr-4'>
+        <div className="flex">
+            <div className="">
+                <div className="inline-block p-2 bg-[#0f5a97]">
+                    <BiBuildings className='h-6 w-auto fill-white'/>
+                </div>
+            </div>
+            <div className="pl-4">
+                <h1 className="text-[#0f5a97] text-xl pt-1 font-semibold uppercase">My Company</h1>
+            </div>
+        </div>
+    </div>
   )
 }
 
