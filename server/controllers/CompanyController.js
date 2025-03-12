124bd3afa43ca9376a5dca7e12512761092c18f1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-12 21:07:56 +0530

    Update CompanyController.js

--- server/controllers/CompanyController.js
+++ server/controllers/CompanyController.js
@@ -1,5 +1,28 @@
 const CompanyController = {
+    createComProfile: async(req, res) => {
+        try{
+            const {
+                com_name,
+                com_email,
+                com_logo,
+                com_cover,
+                com_desc,
+                facebook,
+                linkedin,
+                Twitter,
+                instagram,
+                website,
+                other
+            } = req.body
 
+            const email = res.body.params
+
+            
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
 };
 
 module.exports = CompanyController;
