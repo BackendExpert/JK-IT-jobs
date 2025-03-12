69e6bdffc249a2fd07131e79ee34dfbc3c6f0e9b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-13 02:27:24 +0530

    Update

--- server/controllers/CompanyController.js
+++ server/controllers/CompanyController.js
@@ -5,18 +5,18 @@ const CompanyController = {
         try{
             const {
                 com_name,
-                com_logo,
-                com_cover,
                 com_desc,
                 facebook,
                 linkedin,
-                Twitter,
+                twitter,
                 instagram,
                 website,
                 other
             } = req.body
 
             const com_email = res.body.params
+            const com_logo = req.files['com_logo'] ? req.files['com_logo'][0].path : null;
+            const com_cover = req.files['com_cover'] ? req.files['com_cover'][0].path : null;
 
             const checkCompany = await Company.findOne({
                 $or: [
@@ -29,7 +29,30 @@ const CompanyController = {
                 return res.json({ Error: "Comapany name Already Registed"})
             }
 
-            
+            const newcompany = new Company({
+                com_name: com_name,
+                com_email: com_email,
+                com_logo: com_logo,
+                com_cover: com_cover,
+                com_desc: com_desc,
+                com_links: {
+                    facebook: facebook || "",
+                    linkedin: linkedin || "",
+                    twitter: twitter || "",
+                    instagram: instagram || "",
+                    website: website || "",
+                    other: other || ""
+                }
+            })
+
+            const resultnewcom = await newcompany.save()
+
+            if(resultnewcom){
+                return res.json({ Status: "Success"})
+            }
+            else{
+                return res.json({ Error: "Internal Server Error"})
+            }            
 
         }
         catch(err){
--- server/models/Company.js
+++ server/models/Company.js
@@ -7,12 +7,12 @@ const CompanySchema = new mongoose.Schema({
     com_cover: { type: String, required: true },
     com_desc: { type: String, required: true },
     com_links: {
-        facebook: { type: String },
-        linkedin: { type: String },
-        Twitter: { type: String },
-        instagram: { type: String },
-        website: { type: String },
-        other: { type: String }
+        facebook: { type: String, default: '' },
+        linkedin: { type: String, default: '' },
+        twitter: { type: String, default: '' },
+        instagram: { type: String, default: '' },
+        website: { type: String, default: '' },
+        other: { type: String, default: '' }
     }
 }, { timestamps: true });
 
