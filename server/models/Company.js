590273402bcff61ed619c75becfe64ce39f1df1f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-12 20:57:28 +0530

    Update Company.js

--- server/models/Company.js
+++ server/models/Company.js
@@ -6,9 +6,15 @@ const CompanySchema = new mongoose.Schema({
     com_logo: { type: String, required: true },
     com_cover: { type: String, required: true },
     com_desc: { type: String, required: true },
-    com_links: [{ type: String, required: true }],
-    
-});
+    com_links: {
+        facebook: { type: String },
+        linkedin: { type: String },
+        Twitter: { type: String },
+        instagram: { type: String },
+        website: { type: String },
+        other: { type: String }
+    }
+}, { timestamps: true });
 
 const Company = mongoose.model('Company', CompanySchema);
 
